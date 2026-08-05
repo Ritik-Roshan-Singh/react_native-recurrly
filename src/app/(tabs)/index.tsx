864df85e707775855e9af3e904894f95faca8b2d
Ritik Roshan Singh <111487536+Ritik-Roshan-Singh@users.noreply.github.com>
2026-08-05 11:39:18 +0530

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,43 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import "../../../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href="/Onboarding"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Onboarding
+      </Link>
+
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign Up
+      </Link>
+
+      <Link href="/subscription/spotify">Spotify Subscription</Link>
+      <Link
+        href={{
+          pathname: "/subscription/[id]",
+          params: { id: "claude" },
+        }}
+      >
+        Claude max subscription
+      </Link>
+    </View>
+  );
+}
